654bedb3b1fd470641fef3313f68063db7af0e19
Babai <31babaev@gmail.com>
2017-10-03 15:52:36 +0300

    scripts + new mob btn + new blocks

--- js/js.js
+++ js/js.js
@@ -2,89 +2,51 @@ var $W = $(window),
 	$D = $(document),
 	$H = $('html'),
 	$B = $('body');
-	// swidth=(window.innerWidth-$(window).width());
+	swidth=(window.innerWidth-$(window).width());
 
-// $(function() {
+$(function() {
 
-// 	$('.js-popup-modal').magnificPopup({
-// 		type: 'inline',
-// 		preloader: false,
-// 		focus: '#username',
-// 		showCloseBtn: false,
-// 		mainClass: 'popup-modal-overlay'
-// 		// callbacks: {
-// 		// 	open: function() { $('.header-wr').css('padding-right', swidth + "px"); }, 
-// 		// 	close: function() { $('.header-wr').css('padding-right', 0); },
-// 		// }
-// 	});
+	$('.js-popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+		showCloseBtn: false,
+		mainClass: 'popup-modal-overlay',
+		callbacks: {
+			open: function() { $('.header-wr').css('padding-right', swidth + "px"); }, 
+			close: function() { $('.header-wr').css('padding-right', 0); },
+		}
+	});
 
-// 	$(document).on('click', '.popup-modal-dismiss', function (e) {
-// 		e.preventDefault();
-// 		$.magnificPopup.close();
-// 	});
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+});
 
-// 	$('#select-beast').selectize({
-// 	    create: true,
-// 	    sortField: 'text'
-// 	});
-// });
+$(function() {
 
-// $(function() {
+	$('.js-form').submit(function () {
+		var $form = $(this),
+			$popup = $('#after-modal');
 
-// 	$('.js-form').submit(function () {
-// 		var $form = $(this),
-// 			title = $form.data('title'),
-// 			text = $form.data('text'),
-// 			$popup = $('#after-modal'),
-// 			$popupTitle = $popup.find('.js-popup-title'),
-// 			$popupText= $popup.find('.js-popup-text');
+		$.magnificPopup.close();
+		$.magnificPopup.open({
+			showCloseBtn: false,
+			callbacks: {
+				open: function() { $('.header-wr').css('padding-right', swidth + "px"); }, 
+				close: function() { $('.header-wr').css('padding-right', 0); },
+			},
+			items: {
+				src: '#after-modal'
+			},
+			type: 'inline'
+			}, 
+		0);
+		return false;
+	});
 
-// 		//проверка на успешную отправку
-
-// 		$popupTitle.text(title);
-// 		$popupText.text(text);
-
-// 		$.magnificPopup.close();
-// 		$.magnificPopup.open({
-// 			showCloseBtn: false,
-// 			// callbacks: {
-// 			// 	open: function() { $('.header-wr').css('padding-right', swidth + "px"); }, 
-// 			// 	close: function() { $('.header-wr').css('padding-right', 0); },
-// 			// },
-// 			items: {
-// 				src: '#after-modal'
-// 			},
-// 			type: 'inline'
-// 			}, 
-// 		0);
-// 		return false;
-// 	});
-
-// });
-
-// $(function() {
-
-// 	var $tab = $('.js-tab');
-
-// 	if(!$tab.length) return;
-
-// 	var $tabBtns = $tab.find('.js-btns > div'),
-// 		$tabTabs = $tab.find('.js-tabs > div');
-
-// 	function tab(numb) {
-// 		$tabTabs.removeClass('active')
-// 			.eq(numb).addClass('active');
-
-// 		$tabBtns.removeClass('active')
-// 			.eq(numb).addClass('active');
-// 	}
-
-// 	$tabBtns.bind('click', function () {
-// 		var index = $(this).index();
-// 		tab(index);
-// 	});
-
-// });
+});
 
 $(function() {
 
@@ -94,4 +56,84 @@ $(function() {
 	$faqBtn.click(function(){
 		$(this).next().slideToggle();
 	});
+});
+
+// timer
+$(function() {
+
+	function getTimeRemaining(endtime) {
+	  var t = Date.parse(endtime) - Date.parse(new Date());
+	  var seconds = Math.floor((t / 1000) % 60);
+	  var minutes = Math.floor((t / 1000 / 60) % 60);
+	  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	  return {
+	    'total': t,
+	    'days': days,
+	    'hours': hours,
+	    'minutes': minutes,
+	    'seconds': seconds
+	  };
+	}
+
+	function initializeClock(id, endtime) {
+	  var clock = document.getElementById(id);
+	  var daysSpan = clock.querySelector('.days');
+	  var hoursSpan = clock.querySelector('.hours');
+	  var minutesSpan = clock.querySelector('.minutes');
+	  var secondsSpan = clock.querySelector('.seconds');
+
+	  function updateClock() {
+	    var t = getTimeRemaining(endtime);
+
+	    daysSpan.innerHTML = t.days;
+	    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+	    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+	    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+	    if (t.total <= 0) {
+	      clearInterval(timeinterval);
+	    }
+	  }
+
+	  updateClock();
+	  var timeinterval = setInterval(updateClock, 1000);
+	}
+
+	var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+	initializeClock('clockdiv', deadline);
+});
+// END timer
+
+$(function() {
+
+	var fixed_offset = $('.header-wr').height();
+
+	$(window).resize(function(){
+		fixed_offset = $('.header-wr').height();
+	});
+
+	$(".header-menu").on("click",".header-menu__item a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		$('.header').removeClass('_open');
+		$('.js-mob-menu-btn').removeClass('_open');
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top - fixed_offset}, 1500);
+	});
+});
+
+$(function() {
+
+	$(".js-mob-menu-btn").click(function () {
+		$(this).parent().toggleClass('_open');
+		$(this).toggleClass('_open');
+	});
+	
 });
